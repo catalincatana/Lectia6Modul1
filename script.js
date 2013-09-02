bfe0e37540b3221e2a4323cee9f41501893827ee
catalincatana <catalin.catana3@gmail.com>
2013-09-02 15:48:15 +0300

    Cateva Modificari

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+	$("#Ex1").hide();
+	$("#Ex2").hide();
 	$("img").hide();
+	
+	$( "#Exercitiul1" ).click(function() {
+	$("#Ex1").show();
+	$("#Ex2").hide();
+	});
+	
+	$( "#Exercitiul2" ).click(function() {
+	$("#Ex1").hide();
+	$("#Ex2").show();
+	//$("img").hide();
+	});
+	
 	var checkA = new Array();
 });
