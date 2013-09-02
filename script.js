fa60cbede1635d4fd271ea8f2e61bf618c74e583
Mardaloescu <mardaloescu@mardaloescu-PC.(none)>
2013-09-02 16:53:27 +0300

    smth

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
 	$("#Ex1").hide();
 	$("#Ex2").hide();
 	$("img").hide();
-	
+
 	$( "#Exercitiul1" ).click(function() {
-	$("#Ex1").show();
-	$("#Ex2").hide();
+		$("#Ex1").show();
+		$("#Ex2").hide();
 	});
-	
+
 	$( "#Exercitiul2" ).click(function() {
-	$("#Ex1").hide();
-	$("#Ex2").show();
-	//$("img").hide();
+		$("#Ex1").hide();
+		$("#Ex2").show();
 	});
-	
+
 	var checkA = new Array();
+	
 });
